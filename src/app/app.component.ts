19eec6383fc0be9221f6dce1031885cf02434ce4
Anonymous <ParsWebDesigners@gmail.com>
2022-02-17 19:20:56 +0330

    create subpage menu position absolute & dynamic angle animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,9 +7,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class AppComponent {
   title = 'varkala';
   Menu=false;
-  submenuPage=false;
   @ViewChild('contMenu') menuCont!:ElementRef;
-  @ViewChild('pageSub') pageSub!:ElementRef;
   responsiveMenu(){
     this.Menu=!this.Menu;
     console.log('run')
@@ -18,22 +15,6 @@ export class AppComponent {
       this.menuCont.nativeElement.style.height='auto';
     }else{
       this.menuCont.nativeElement.style.height=0;
-    }
-  }
-  togglePage(){
-    this.submenuPage=!this.submenuPage;
-    if(this.submenuPage){
-      this.pageSub.nativeElement.classList.add('mt-32');
-      this.pageSub.nativeElement.classList.remove('mt-44');
-      this.pageSub.nativeElement.style.opacity=1;
-      this.pageSub.nativeElement.style.pointerEvents='all';
-    }else{
-      this.pageSub.nativeElement.classList.add('mt-44');
-      this.pageSub.nativeElement.classList.remove('mt-32');
-      setTimeout(()=>{
-        this.pageSub.nativeElement.style.opacity=0;
-        this.pageSub.nativeElement.style.pointerEvents='none';
-      },100)
     }
   }
   

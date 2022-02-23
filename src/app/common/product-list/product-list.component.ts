878f7db687a8e8bf3708a51f1fb26dd9f26e59ac
Anonymous <ParsWebDesigners@gmail.com>
2022-02-23 16:16:21 +0330

    install AOS and use it in product items

--- src/app/common/product-list/product-list.component.ts
+++ src/app/common/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-product-list',
@@ -10,6 +11,7 @@ export class ProductListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    AOS.init();
   }
 
 }
